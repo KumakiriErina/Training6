49e4dbfb640be2e4fdcbeb6b26e9fe9150f0e26c
e_kumakiri <e_kumakiri@192.168.0.5>
2023-12-30 21:28:31 +0900

    入力チェックのメッセージを日本語に変更しました。

--- src/main/java/training6/form/ResultForm.java
+++ src/main/java/training6/form/ResultForm.java
@@ -2,6 +2,7 @@ package training6.form;
 
 import java.util.List;
 
+import org.seasar.struts.annotation.Arg;
 import org.seasar.struts.annotation.DateType;
 import org.seasar.struts.annotation.Mask;
 import org.seasar.struts.annotation.Required;
@@ -14,9 +15,9 @@ import training6.omikuji.Omikuji;
 public class ResultForm {
 
 	/**誕生日のプロパティです*/
-	@Required
-	@Mask(mask = "^[0-9]{4}[0-9]{2}[0-9]{2}$")
-	@DateType(datePatternStrict = "yyyyMMdd")
+	@Required(arg0 = @Arg(key = "誕生日", resource = false))
+	@Mask(mask = "^[0-9]{4}[0-9]{2}[0-9]{2}$", arg0 = @Arg(key="誕生日", resource = false))
+	@DateType(datePatternStrict = "yyyyMMdd", arg0 = @Arg(key="誕生日", resource = false))
 	public String birthday;
 	
 	/**おみくじの結果のプロパティです*/
